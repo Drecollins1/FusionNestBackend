27d6a9d9002d759c33fecf56e2a1c92bf7d4c408
Drecollins1 <adarecollins@gmail.com>
2024-12-11 09:29:49 +0100

    Update CORS configuration in middlewares.js

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,48 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:", "http:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "https://fusionnestbackend-1.onrender.com",
+            "http://localhost:1337",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "https://fusionnestbackend-1.onrender.com",
+            "http://localhost:1337",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      origin: [
+        "http://localhost:3000", // Local React app for development
+        "https://your-live-react-app-domain.com", // Replace with your live React app's domain
+      ],
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
